285e70cab4a2b33a624e2cb3b6659a1192cbcd25
cjc <dj123456>
2021-08-05 21:44:32 +0800

    ss

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 加载element组件库
+import ElementUI from 'element-ui'
+// 加载 element组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
 
+// 加载全局样式文件
+import './styles/index.less'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
-
+// 创建Vue 根实例
+// 把router 配置到根实例中
+// 通过render 方法把APP根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
